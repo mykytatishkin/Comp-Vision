9ff1b40983807b812674791af1df0342a8e0e1e1
Demian Ruban <rubandemian@gmail.com>
2022-04-15 10:50:28 +0200

    Code for api fetching

--- app.js
+++ app.js
@@ -1,29 +1,62 @@
 function sendImg() {
 
-    if (dropArea.getElementsByTagName("img").length == 0) { // check if user loaded image
-	const main = document.querySelectorAll("main")[0];
-	// Create error text
-	const para = document.createElement("p");
-	const node = document.createTextNode("There is no image loaded!");
-	para.setAttribute("class", "text-danger bg-dark text-center");
-	para.appendChild(node);
-	main.appendChild(para);
+    if (dropArea.getElementsByTagName("img").length == 0) { // check if user didn't load an image
+	if (!document.getElementById("noImageError")) { // check if error message don't exist yet
+	    const main = document.querySelectorAll("main")[0];
+	    // Create error text
+	    const para = document.createElement("p");
+	    const node = document.createTextNode("There is no image loaded!");
+	    para.setAttribute("class", "text-danger bg-dark text-center");
+	    para.setAttribute("id", "noImageError");
+	    para.appendChild(node);
+	    main.appendChild(para);
+	}
+    } else {
+	imgUrl = dropArea.getElementsByTagName("img")[0].getAttribute("src"); // getting img url
+	console.log("This is a url in image");
+	console.log(imgUrl);
+	//imgUrl = imgUrl.substring(22, imgUrl.length); // decision from stackoverflow doesn't work :(
+	//console.log(imgUrl);
 
+	imageInfo = document.getElementById("imageInfo"); // div that containes image info
+	header = imageInfo.childNodes[1]; // No Info header
+
+	const options = {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/json',
+			'X-RapidAPI-Host': 'image-labeling1.p.rapidapi.com',
+			'X-RapidAPI-Key': '1a8dcf20a0mshc5346be1d4720c5p1fb685jsnb29c9f6774e6' // paste here api key secretly
+		},
+		body: `{"url":"${imgUrl}"}`
+		//body: '{"url":"https://www.inferdo.com/img/label-1.jpg"}' // original string file from server
+	};
+
+	/* currently there is an error here: POST 400 bad request
+	 * when getting response the api server returns text with
+	 * a message that states: Request image failed to download: failed to parse
+	 * maybe it's that the server adds http:// string at the beginning
+	 * that maybe is the cause
+	 * Because server response with a text, there is complains with JSON
+	 * syntax.
+	 */
+	console.log("Sending imgUrl and getting response...");
+	fetch('https://image-labeling1.p.rapidapi.com/img/label', options) // getting response
+		.then(response => response.text()) // it must be response.json()
+		.then(response => console.log(response))
+		//.then(response => {
+		//    console.log(response);
+		//    header.textContent = "Info";
+		//    for (const label in response) { //enumerating (iterating through indexes)
+		//	let match = response[label];
+		//	let labelPara = document.createElement('p')
+		//	labelPara.textContent = label + " " + match;
+		//	imageInfo.appendChild(labelPara);
+		//    }
+		//})
+		.catch(err => console.error(err));
     }
-//    const options = {
-//	    method: 'POST',
-//	    headers: {
-//		    'content-type': 'application/json',
-//		    'X-RapidAPI-Host': 'image-labeling1.p.rapidapi.com',
-//		    'X-RapidAPI-Key': ''
-//	    },
-//	    body: '{"url":"./download.jpeg"}'
-//    };
-//
-//    fetch('https://image-labeling1.p.rapidapi.com/img/label', options)
-//	    .then(response => response.json())
-//	    .then(response => console.log(response))
-//	    .catch(err => console.error(err));
+
 }
 
 //selecting all required elements
@@ -73,6 +106,7 @@ function showFile() {
         let fileReader = new FileReader(); //creating new FileReader object
         fileReader.onload = () => {
             let fileURL = fileReader.result; //passing user file source in fileURL variable
+
             let imgTag = `<img src="${fileURL}" alt="">`; //creating an img tag and passing user selected file source inside src attribute
             dropArea.innerHTML = imgTag; //adding that created img tag inside dropArea container
         }
